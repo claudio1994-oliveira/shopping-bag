7c373f61cb258b3d17657e60611ab6a448e4a2cb
Cláudio Oliveira <franciscoclaudiooliveira@gmail.com>
2024-01-06 23:18:25 -0300

    remover produto na pag de fin. compra

--- src/views/ShoppingBasket.vue
+++ src/views/ShoppingBasket.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="basket">
     <div class="items">
-
-      <div class="item">
-        <div class="remove">Remover Produto</div>
-        <div class="photo"><img src="https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg" alt=""></div>
-        <div class="description">Mens Casual Premium Slim Fit T-Shirts </div>
-        <div class="price">
-          <span class="quantity-area">
-            <button disabled="">-</button>
-            <span class="quantity">1</span>
-            <button>+</button>
-          </span>
-          <span class="amount">R$ 22.30</span>
+      <div v-for="(product, index) in this.productsInBag" :key="index">
+        <div class="item">
+          <div class="remove" @click="this.$store.dispatch('removeFromBag', product)">Remover Produto</div>
+          <div class="photo"><img :src="product.image" alt=""></div>
+          <div class="description">{{ product.title }} </div>
+          <div class="price">
+            <span class="quantity-area">
+              <button :disabled="product.quantity <=1" @click="product.quantity--">-</button>
+              <span class="quantity">{{ product.quantity }}</span>
+              <button @click="product.quantity++">+</button>
+            </span>
+            <span class="amount">R$ {{ (product.price * product.quantity).toFixed(2) }}</span>
+          </div>
         </div>
       </div>
       <div class="grand-total"> Total do pedido: R$ 22.30</div>
@@ -22,6 +23,7 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
 
 export default {
   name: 'ShoppingBasket',
@@ -29,6 +31,7 @@ export default {
   methods: {
    
   },
+  computed: mapState(['productsInBag']),
  
 }
 </script>
